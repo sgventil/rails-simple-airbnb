0c8ef566cd28b83f8a4a87d3e3f399ae7700b8d4
Fernando Ribeiro <pontogponto@gmail.com>
2023-08-01 14:54:59 +0200

    added show # edit # update # destroy # image

--- app/controllers/flats_controller.rb
+++ app/controllers/flats_controller.rb
@@ -1,10 +1,11 @@
 class FlatsController < ApplicationController
+  before_action :set_flat, only: [:show, :edit, :update, :destroy]
+
   def index
     @flats = Flat.all
   end
 
   def show
-    @flat = Flat.find(params[:id])
   end
 
   def new
@@ -20,10 +21,29 @@ class FlatsController < ApplicationController
     end
   end
 
+  def edit
+  end
+
+  def update
+    if @flat.update(flat_params)
+      redirect_to flat_path(@flat)
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @flat.destroy
+    redirect_to flats_url, notice: "Flat was successfully removed.", status: :see_other
+  end
+
   private
+
+  def set_flat
+    @flat = Flat.find(params[:id])
+  end
 
   def flat_params
     params.require(:flat).permit(:name, :address, :description, :number_of_guests, :price_per_night)
   end
-
 end
--- app/models/flat.rb
+++ app/models/flat.rb
@@ -1,2 +1,3 @@
 class Flat < ApplicationRecord
+  validates :name, :address, :number_of_guests, :price_per_night, presence: true
 end
